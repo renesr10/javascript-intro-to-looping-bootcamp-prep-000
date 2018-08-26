a7fd3bb59e7c647c9c77c4bc2e9203b93ef48b09
Rene Santana <renesantana10@gmail.com>
2018-08-26 05:49:11 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array) {for (let i = 0; i < 25; i++) {
+  if (i === 1) { array.push("I am 1 strange loop."); } else  { array.push(`I am ${i} strange loops.`); } }
+  return array 
+}
+function whileLoop(n) { let countdown = 100; {while (countdown > n) { console.log(--countdown);
+  return 'done';
+}
+}
+}
+
+
+
+
+//function forLoop(array) {
+  //for (let i = 0; i < 25; i++) {
+    //if (i === 1) {
+     // array.push("I am 1 strange loop.");
+    //} else {
+      //array.push(`I am ${i} strange loops.`);
+    //}
+  //}
+  //return array;
+//}

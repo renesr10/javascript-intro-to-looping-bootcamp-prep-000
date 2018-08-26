4e44ec476e4cba04dc548005b36f5abdbd66c063
Rene Santana <renesantana10@gmail.com>
2018-08-26 07:03:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,6 @@ function forLoop(array) {for (let i = 0; i < 25; i++) {
   if (i === 1) { array.push("I am 1 strange loop."); } else  { array.push(`I am ${i} strange loops.`); } }
   return array 
 }
-function whileLoop(n) {while (console.log(--n));
+function whileLoop(n) {while (console.log('count down ',--n));
   return 'done';
 }
